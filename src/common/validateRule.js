118fe14478019d4a081738c123a69aad0bbce3a7
kavindasilva <kavinda@vetstoria.com>
2019-07-30 16:23:32 +0530

    upto 14 test cases

--- src/common/validateRule.js
+++ src/common/validateRule.js
@@ -3,7 +3,9 @@ import Peg from "../parsers/conditionsParser"
 import { throws } from "assert";
 
 const definedFunctions = [
-    "moment"
+    "moment",
+    "isBefore",
+    "isAfter"
 ];
 
 
@@ -15,7 +17,7 @@ export const validateExpression = function ( expr ){
         //console.log("Parse Tree", parseTree);
     }
     catch (ex) {
-        console.log("ex",  ex);
+        //console.log("ex1",  ex);
         throw ex;
         //return true;
     }
@@ -23,31 +25,63 @@ export const validateExpression = function ( expr ){
     try {
         validateSubTree(parseTree);
     } catch (ex) {
+        //console.log("ex2",  ex);
         throw ex;
     }
 
     return parseTree;
 }
 
-function validateFunctionSubTree(subTree) {
-    if ((subTree !== null) 
-        && (typeof subTree === 'object')
-        && (subTree.type == 'function')) {
-            if (-1 == definedFunctions.indexOf(subTree.name)) {
-                throw new Error(`Undefined Function '{subTree.name}'`);
-            }
-    }
-}
-
 function validateSubTree(subTree) {
-    if (Array.isArray(subTree)) {
+    if (Array.isArray(subTree)) { //if s
         subTree.forEach(element => {
             validateSubTree(element);
         });
     }
 
-    validateFunctionSubTree(subTree);
-    //validate string
+    if( 
+        !validateFunctionSubTree(subTree) 
+        && !validateString(subTree)
+        && !validateNumber(subTree)
+    ){
+        throw new Error(`unknown type ${subTree.toString()}`);
+    }
     //validate integer 
     
+    
 }
+
+function validateString(subTree){
+    if( subTree !== null
+        && (typeof subTree)==="object"
+        && (subTree.type === "string")
+    ){
+        return true;
+    }
+    return false;
+}
+
+function validateNumber(subTree){
+    if( subTree !== null
+        && (typeof subTree)==="object"
+        && (subTree.type === "number")
+    ){
+        return true;
+    }
+    return false;
+}
+
+function validateFunctionSubTree(subTree) {
+    if ((subTree !== null) 
+        && (typeof subTree === 'object')
+        && (subTree.type == 'function')
+    ){
+        if (-1 === definedFunctions.indexOf(subTree.name)) {
+            //throw new Error("Undefined Function");
+            throw new Error(`Undefined Function: ${subTree.name}`);
+        }
+        return true;
+    }
+    return false;
+}
+
--- test/validateRule.test.js
+++ test/validateRule.test.js
@@ -1,22 +1,15 @@
+
 import {sum, validateExpression} from "../src/common/validateRule"
 
+let expressionToEvaluate = null
+/**
+ * positive test cases
+ * function to check: validateExpression
+ */
 
-test('a pure string expression will fail to parse', () => {
-    expect(() => {
-        validateExpression("blahblahblah");
-    }).toThrow();
-});
-
-
-test('expression "undefinedFunction()" will throw an "Undefined Function" error', () => {
-    expect(() => {
-        validateExpression("undefinedFunction()");
-    }).toThrow("Undefined Function");
-});
-
-
+/** moment() evaluation */
 test('expression "moment()" will give a valid parse tree', () => {
-    expect(
+    expect( 
         validateExpression("moment()")
     )
     .toMatchObject({
@@ -26,19 +19,233 @@ test('expression "moment()" will give a valid parse tree', () => {
     });
 });
 
-// test('expression "myFunction(a)" will give a valid parse tree', () => {
-//     expect(
-//         validateExpression("myFunction('a')")
-//     )
-//     .toMatchObject({
-//         type        : 'function',
-//         name        : 'myFunction',
-//         parameters  : [
-//             {
-//                 type: 'string',
-//                 value: 'a'
-//             }
-//         ]
-//     });
-// });
+/** vaild function with parameter */
+test('expression "moment(a)" will give a valid parse tree', () => {
+    expect(
+        validateExpression("moment('a')")
+    )
+    .toMatchObject({
+        type        : 'function',
+        name        : 'moment',
+        parameters  : [
+            {
+                type: 'string',
+                value: 'a'
+            }
+        ]
+    });
+});
 
+/** valid function with more than one string parameter */
+test('expression "moment(\'1\', \'b\', \'c\')" will give a valid parse tree', () => {
+    expect(
+        validateExpression("moment('1', 'b', 'cc')")
+    )
+    .toMatchObject({
+        type        : 'function',
+        name        : 'moment',
+        parameters  : [
+            {
+                type: 'string',
+                value: '1'
+            },
+            {
+                type: 'string',
+                value: 'b'
+            },
+            {
+                type: 'string',
+                value: 'cc'
+            }
+        ]
+    });
+});
+
+/** valid function with more than one integer parameter */
+test('expression "moment(1, 2, 3)" will give a valid parse tree', () => {
+    expect(
+        validateExpression("moment(1, 2, 3)")
+    )
+    .toMatchObject({
+        type        : 'function',
+        name        : 'moment',
+        parameters  : [
+            {
+                type: 'number',
+                value: 1
+            },
+            {
+                type: 'number',
+                value: 2
+            },
+            {
+                type: 'number',
+                value: 3
+            }
+        ]
+    });
+});
+
+/** valid function with mixed parameters */
+test('expression "moment(1, isBefore(2))" will give a valid parse tree', () => {
+    expect(
+        validateExpression("moment(1, isBefore(2))")
+    )
+    .toMatchObject({
+        "type": "function",
+        "name": "moment",
+        "parameters": [
+            {
+                "type": "number",
+                "value": 1
+            },
+            {
+                "type": "function",
+                "name": "isBefore",
+                "parameters": [
+                    {
+                        "type": "number",
+                        "value": 2
+                    }
+                ]
+            }
+        ]
+    });
+});
+
+/** valid function with mixed parameters */
+test('expression "moment(1, "sample", isBefore(2))" will give a valid parse tree', () => {
+    expect(
+        validateExpression("moment(1, 'sample', isBefore(2))")
+    )
+    .toMatchObject({
+        "type": "function",
+        "name": "moment",
+        "parameters": [
+            {
+                "type": "number",
+                "value": 1
+            },
+            {
+                "type": "string",
+                "value": "sample"
+            },
+            {
+                "type": "function",
+                "name": "isBefore",
+                "parameters": [
+                    {
+                        "type": "number",
+                        "value": 2
+                    }
+                ]
+            }
+        ]
+    });
+});
+
+/** valid function with mixed parameters */
+test('expression "moment(1, isBefore(isTrue(5,5), 3))" will give a valid parse tree', () => {
+    expect(
+        validateExpression("moment(1, isBefore(isTrue(5, '5'), 3))")
+    )
+    .toMatchObject({
+        "type": "function",
+        "name": "moment",
+        "parameters": [
+            {
+                "type": "number",
+                "value": 1
+            },
+            {
+                "type": "function",
+                "name": "isBefore",
+                "parameters": [
+                    {
+                        "type": "function",
+                        "name": "isTrue",
+                        "parameters":[
+                            {
+                                "type": "number",
+                                "value": 5
+                            },
+                            {
+                                "type": "string",
+                                "value": "5"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "number",
+                        "value": 3
+                    }
+                ]
+            }
+        ]
+    });
+});
+
+
+/**
+ * negative test cases
+ */
+
+
+/** passing empty input */
+test('empty expression should throw error', () => {
+    expect( () =>
+        validateExpression("")
+    )
+    .toThrow( /^Expected function but/) ;
+});
+
+/** passing a null input */
+test('null expression should throw error', () => {
+    expect( () =>
+        validateExpression() // null gives -> Cannot read property 'charAt' of null
+    )
+    //.toThrow();
+    .toThrow("Cannot read property 'charAt' of undefined") ;
+});
+
+ /** pure string evaluation */
+test('a pure string expression will fail to parse', () => {
+    expect(() => {
+        validateExpression("blahblahblah");
+    }).toThrow();
+});
+
+/** undefined function */
+test('expression "functionFour(a)" will throw an undefined', () => {
+    expect( ()=>
+        validateExpression("functionFour('a')")
+    )
+    .toThrow("Undefined Function");
+});
+
+/** invalid function with parameters as array */
+test('expression "moment(1, [2, 3])" will throw an error like "Expected function but..."', () => {
+    let err=null;
+    expect( () =>
+        validateExpression("moment(1, [2, 3])")
+    )
+    .toThrow( /^Expected function but/) ;
+    //err.toMatch(/^Expected function but/);
+});
+
+/** passing just a value makes an error */
+test('expression "stringValue" will throw an error like "Expected function but..."', () => {
+    let err=null;
+    expect( () =>
+        validateExpression("stringValue")
+    )
+    .toThrow( /^Expected function but/) ;
+});
+
+/** passing invalid string */
+test('expression "moment(1, invalidString)" will throw error', () => {
+    expect( () =>
+        validateExpression("moment(1, invalidString)")
+    )
+    .toThrow( /^Expected function but/) ;
+});
